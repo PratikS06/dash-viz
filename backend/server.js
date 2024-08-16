5a966067e84975ebcd491e335951cf16f40df1ef
Pratik <pratiksawant3634@gmail.com>
2024-08-16 13:42:10 +0530

    deploy 2nd

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const {connectDB} = require('./db/config')
 const dataRoute = require('./routes/data')
+const path  = require('path')
 
 const app = express()
 
@@ -10,10 +11,19 @@ dotenv.config()
 
 const port  = process.env.PORT || 3000
 
+const dirname = path.resolve();
+
 app.use(cors())
 app.use(express.json())
 
+
 app.use('/api',dataRoute)
+
+app.use(express.static(path.join(dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(dirname, 'frontend', 'dist', 'index.html'));
+});
 
 app.listen(port,()=>{
     connectDB()
